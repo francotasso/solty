d1e59900b8732598de4e17c8622377326697f724
Franco <42650227+francotasso@users.noreply.github.com>
2019-08-23 04:38:37 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'main/build.js'
   },
   module: {
     rules: [
