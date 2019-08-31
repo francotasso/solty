932db6308239f0c5f21f0d95e8ab98acf24fede4
Franco <42650227+francotasso@users.noreply.github.com>
2019-08-30 21:22:19 -0500

    Update AboutUs.vue

--- src/Components/AboutUs.vue
+++ src/Components/AboutUs.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <navbar />
-    <div class="container d-flex flex-row">
-      <div class="box d-flex justify-content-center">
-        <img
-          src="../assets/AboutImage2.png"
-          alt="Coreana a la moda"
-          class="img-fluid d-none d-md-block"
-        />
-      </div>
-      <div class="content-right">
-        <h1>Solty tu tienda Online</h1>
-        <p>Solty es una empresa de venta de ropa online con presencia en todo el Perú y gran parte de Latinoamérica</p>
-        <p>Desde nuestros inicios hemos optado siempre por la calidad de la ropa que vendemos a nuestros clientes haciéndolos sentir seguros con su produtos y generando una buena imagen tanto para ellos mismos como para nosotros mismos.</p>
-        <p>No esperes más y opta por nuestras más de 5000 prendas de vestir de la mejor calidad que te harán lucir mejor.</p>
+    <div class="container">
+      <div class="row">
+       <div class="box col-md-6 d-flex justify-content-center">
+         <img
+           src="../assets/AboutImage2.png"
+           alt="Coreana a la moda"
+           class="img-fluid d-none d-md-block"
+         />
+       </div>
+       <div class="content-right col-md-6">
+         <h1>Solty tu tienda Online</h1>
+         <p>Solty es una empresa de venta de ropa online con presencia en todo el Perú y gran parte de Latinoamérica</p>
+         <p>Desde nuestros inicios hemos optado siempre por la calidad de la ropa que vendemos a nuestros clientes haciéndolos sentir seguros con su produtos y generando una buena imagen tanto para ellos mismos como para nosotros mismos.</p>
+         <p>No esperes más y opta por nuestras más de 5000 prendas de vestir de la mejor calidad que te harán lucir mejor.</p>
+       </div>
       </div>
     </div>
     <foot />
