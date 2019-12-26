729103983f60c0465671daff60ad361f0a6b0281
Tasso <42650227+francotasso@users.noreply.github.com>
2019-12-25 23:55:23 -0500

    Modeling getProducts to Promise

--- src/Services/ProductService.js
+++ src/Services/ProductService.js
@@ -8,17 +8,15 @@ export const productService = {
 
 function getProducts(numPage) {
     let url = URL.url.concat(`/products/${numPage}`);
-    return axios
-        .get(
-            url,
-            { withCredentials: true }
-        )
+    return new Promise((resolve, reject) => {
+      axios.get(url, { withCredentials: true })
         .then(res => {
-            return res.data;
+            resolve(res.data)
         })
-        .catch(err => {
-            return Promise.reject(new Error(err));
+        .catch(error => {
+            reject(new Error(`Error ${error}`));
         });
+    })
 };
 
 function getProduct(productId) {
